5724c8828eb2e7c5a734692eca2cbffe6f870ecb
akalankapagoda <akalankapagoda@gmail.com>
2022-05-31 06:13:30 +0530

    Initial commit : create project structure

---
+++ src/main/java/com/paymentology/aka/recon/DataReconciliationApplication.java
@@ -0,0 +1,13 @@
+package com.paymentology.aka.recon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataReconciliationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataReconciliationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymentology/aka/recon/DataReconciliationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentology.aka.recon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataReconciliationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
